12673a090ee40c314a13805063b2a3e0e1d5e8bc
connorwarme <connor.warme@gmail.com>
2023-01-12 12:46:25 -0700

    Write a file w/ fs/Promises module; append to file (regular and w/ promises)

--- app.js
+++ app.js
@@ -10,4 +10,36 @@ fs.writeFile('/home/peregrinning/Documents/Coding/TOP/learn-node/initial.txt', c
     }
 })
 // worked!!
-
+//
+// write files via fsPromises module
+const fsp = require('fs/promises');
+const example = async () => {
+    try {
+        const content = 'Write a file with fs/promises module!';
+        await fsp.writeFile('/home/peregrinning/Documents/Coding/TOP/learn-node/promises.txt', content);
+    } catch (err) {
+        console.log(err);
+    }
+}
+example();
+// worked!!
+//
+// append to a file
+const appendContent = ' Append this to a file!';
+fs.appendFile('/home/peregrinning/Documents/Coding/TOP/learn-node/initial.txt', appendContent, err => {
+    if (err) {
+        console.log(err);
+    }
+})
+// worked!!
+//
+// append to a file via fsPromises
+const append = async () => {
+    try {
+        await fsp.appendFile('/home/peregrinning/Documents/Coding/TOP/learn-node/promises.txt', appendContent);
+    } catch (err) {
+        console.log(err);
+    }
+}
+append();
+// worked!!
